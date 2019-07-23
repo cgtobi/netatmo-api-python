29e1586fee91a198430004e4f9a5c6634bc7b199
Tobias Sauerwein <cgtobi@gmail.com>
2019-07-23 15:55:48 +0200

    Add station id parameter

--- smart_home/WeatherStation.py
+++ smart_home/WeatherStation.py
@@ -62,8 +62,8 @@ class WeatherStationData:
                 res.add(station["module_name"])
         return list(res)
 
-    def stationByName(self, station=None):
-        if not station:
+    def stationByName(self, station=None, station_id=None):
+        if not station and not station_id:
             station = self.default_station
         for i, s in self.stations.items():
             if s["station_name"] == station:
