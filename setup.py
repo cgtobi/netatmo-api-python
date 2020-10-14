69a35839ed63930b6662c0bf405eb45ed24d2a3b
Tobias Sauerwein <cgtobi@gmail.com>
2020-10-14 13:02:27 +0200

    Fix typo

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     license="MIT",
     description=(
         "Simple API to access Netatmo weather station data from any Python 3 script. "
-        "Design for Home-Assitant (but not only)"
+        "Designed for Home-Assitant (but not only)"
     ),
     long_description=open("README.md").read(),
     install_requires=["requests", "requests_oauthlib", "oauthlib"],
