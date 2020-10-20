9251ad72f7840be93694e803b7404ee536087095
Tobias Sauerwein <cgtobi@gmail.com>
2020-10-20 18:22:50 +0200

    Move version file

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 # python setup.py --dry-run --verbose install
 # To build: python setup.py sdist bdist_wheel
 
+from distutils.util import convert_path
+
 from setuptools import find_packages, setup
 
-from version import __version__
+MAIN_NS = {}
+VER_PATH = convert_path("src/pyatmo/version.py")
+with open(VER_PATH) as ver_file:
+    exec(ver_file.read(), MAIN_NS)  # pylint: disable=exec-used
 
 setup(
     name="pyatmo",
-    version=__version__,
+    version=MAIN_NS["__version__"],
     author="Hugo Dupras",
     author_email="jabesq@gmail.com",
     packages=find_packages(exclude=["tests"], where="src"),
-    package_dir={"": "src"},
+    package_dir={"pyatmo": "src/pyatmo"},
     scripts=[],
-    data_files=[],
+    data_files=[("", ["LICENSE.txt"])],
     url="https://github.com/jabesq/netatmo-api-python",
     license="MIT",
     description=(
